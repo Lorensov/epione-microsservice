9f74eb40bc5a927602607d9c905c7bd03378694b
Lorensov <lucianlorens@gmail.com>
2018-06-29 00:53:46 -0300

    added __init__

--- src/run/UbsRun.py
+++ src/run/UbsRun.py
@@ -4,4 +4,20 @@ import src.service.UbsService
 
 print("test")
 
+#*MUST
+#receive solicitation *
+
+#*MUST
+#make a request for the archives from the site 
+#easy flux: use the local file 
+
+#save to cache
+
+#fix file
+
+#return json *MUST
+
+#saves to mongoDB
+
+
 
