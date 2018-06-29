034aa30886bff99a29e019711bc9b34925b825b8
Lorensov <lucianlorens@gmail.com>
2018-06-29 04:28:21 -0300

    Added basic structure to json handling

---
+++ src/service/fileDownloader/webGet.py
@@ -0,0 +1,16 @@
+import urllib
+import urllib.request
+
+def getHTML(url):
+    print(url)
+
+def downloadFile(url, file_name):
+# Download the file from `url` and save it locally under `file_name`:
+    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        data = response.read() # a `bytes` object
+        print("downloading!")
+        out_file.write(data)
+    print("DONE!")
+
+downloadFile( "http://repositorio.dados.gov.br/saude/unidades-saude/unidade-basica-saude/ubs.csv", "resources/ubs.csv" )
+
